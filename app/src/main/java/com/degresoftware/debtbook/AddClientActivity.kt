668149d9918a3bbaec5b2f3c85c8c4c5f39f6196
dincerdegre <dincerdegre@gmail.com>
2021-10-12 00:21:39 +0300

    First Commit

---
+++ app/src/main/java/com/degresoftware/debtbook/AddClientActivity.kt
@@ -0,0 +1,11 @@
+package com.degresoftware.debtbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddClientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_client)
+    }
+}
