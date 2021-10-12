c0bf4f7d68418c47c76f99d67549ede12215d4b6
dincerdegre <dincerdegre@gmail.com>
2021-10-12 23:23:20 +0300

    12.10.2021 Commit

--- app/src/main/java/com/degresoftware/debtbook/AddClientActivity.kt
+++ app/src/main/java/com/degresoftware/debtbook/AddClientActivity.kt
@@ -1,11 +1,132 @@
 package com.degresoftware.debtbook
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.sqlite.SQLiteDatabase
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.degresoftware.debtbook.databinding.ActivityAddClientBinding
+import com.google.android.material.snackbar.Snackbar
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.lang.Exception
+import java.util.jar.Manifest
 
 class AddClientActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAddClientBinding
+    var chooseBitmap : Bitmap? = null
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var database : SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_client)
+        binding = ActivityAddClientBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        database = this.openOrCreateDatabase("DebtBook", Context.MODE_PRIVATE,null)
+        registerLauncher()
+    }
+
+
+
+    fun selectPicture(view: View){
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view,"Permission Needed",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", View.OnClickListener {
+                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }).show()
+            } else {
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intentToGallery)
+        }
+    }
+
+    private fun registerLauncher() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intentFromResult = result.data
+                if (intentFromResult != null) {
+                    val imageData = intentFromResult.data
+                    try {
+                        if (Build.VERSION.SDK_INT >= 28) {
+                            val source = ImageDecoder.createSource(this@AddClientActivity.contentResolver, imageData!!)
+                            chooseBitmap = ImageDecoder.decodeBitmap(source)
+                            binding.clientPicture.setImageBitmap(chooseBitmap)
+                        } else {
+                            chooseBitmap = MediaStore.Images.Media.getBitmap(this@AddClientActivity.contentResolver, imageData)
+                            binding.clientPicture.setImageBitmap(chooseBitmap)
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                //permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            } else {
+                //permission denied
+                Toast.makeText(this@AddClientActivity, "Permission needed!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun save(view: View){
+        val clientName = binding.clientNameText.text.toString()
+        if (chooseBitmap != null){
+            val smallBitmap = makeSmallerBitmap(chooseBitmap!!,300)
+            val outputStream = ByteArrayOutputStream()
+            smallBitmap.compress(Bitmap.CompressFormat.PNG,50,outputStream)
+            val byteArray = outputStream.toByteArray()
+            try {
+                database.execSQL("CREATE TABLE IF NOT EXISTS clients (id INTEGER PRIMARY KEY, clientName VARCHAR, clientPicture BLOB)")
+                val sqlString = "INSERT INTO clients (clientName,clientPicture) VALUES (?,?)"
+                val statement = database.compileStatement(sqlString)
+                statement.bindString(1,clientName)
+                statement.bindBlob(2,byteArray)
+                statement.execute()
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+            val intent = Intent(this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+    }
+
+
+
+    fun makeSmallerBitmap(image: Bitmap, maximumSize : Int) : Bitmap {
+        var width = image.width
+        var height = image.height
+
+        val bitmapRatio : Double = width.toDouble() / height.toDouble()
+        if (bitmapRatio > 1) {
+            width = maximumSize
+            val scaledHeight = width / bitmapRatio
+            height = scaledHeight.toInt()
+        } else {
+            height = maximumSize
+            val scaledWidth = height * bitmapRatio
+            width = scaledWidth.toInt()
+        }
+        return Bitmap.createScaledBitmap(image,width,height,true)
     }
 }
--- app/src/main/java/com/degresoftware/debtbook/MainActivity.kt
+++ app/src/main/java/com/degresoftware/debtbook/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.degresoftware.debtbook
 
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.degresoftware.debtbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var database : SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        database = this.openOrCreateDatabase("DebtBook", Context.MODE_PRIVATE,null)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_client,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_client){
+            val intent = Intent(this,AddClientActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
